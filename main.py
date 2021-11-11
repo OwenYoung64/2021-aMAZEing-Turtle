3ba98895043cd6f73a7ddbdbecd35228d7de9591
Owen Young <owen.young@vtsd.com>
2021-11-11 11:46:06 -0500

    Commit

--- main.py
+++ main.py
@@ -5,14 +5,30 @@ maze_maker = trtl.Turtle()
 maze_maker.goto(0, 0)
 maze_maker.speed(0)
 maze_maker.left(90)
-maze_maker.pensize(3)
+maze_maker.pensize(5)
 numlines = 0
-line_length = 30
+line_length = 50
+door_size = 30
+before_door = 20
+block_size = 20
 #Draw the maze
-while(numlines <= 25):
-    maze_maker.forward(line_length)
+while(numlines <= 24):
+    if (numlines < 4):
+        maze_maker.forward(line_length)
+    else:
+        maze_maker.forward(before_door)
+        maze_maker.penup()
+        maze_maker.forward(door_size)
+        maze_maker.pendown()
+        #barriers
+        maze_maker.right(90)
+        maze_maker.forward(block_size)
+        maze_maker.right(180)
+        maze_maker.forward(block_size)
+        maze_maker.right(90)
+        maze_maker.forward(line_length - door_size - before_door - block_size)
     maze_maker.right(90)
-    line_length += 10
+    line_length += 20
     numlines += 1
 
 
