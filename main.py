14eec399b547e465f72877320d796ac028829d10
Owen Young <owen.young@vtsd.com>
2021-11-12 13:43:29 -0500

    Commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle as trtl
+import random as rand
 #Set up variables
 wn = trtl.Screen()
 maze_maker = trtl.Turtle()
@@ -10,10 +11,10 @@ numlines = 0
 line_length = 50
 door_size = 30
 before_door = 20
-block_size = 20
+block_size = 40
 #Draw the maze
 while(numlines <= 24):
-    if (numlines < 4):
+    if (numlines < 6):
         maze_maker.forward(line_length)
     else:
         maze_maker.forward(before_door)
@@ -21,20 +22,16 @@ while(numlines <= 24):
         maze_maker.forward(door_size)
         maze_maker.pendown()
         #barriers
+        maze_maker.forward(before_door * 2.5)
         maze_maker.right(90)
         maze_maker.forward(block_size)
         maze_maker.right(180)
         maze_maker.forward(block_size)
         maze_maker.right(90)
-        maze_maker.forward(line_length - door_size - before_door - block_size)
+        maze_maker.forward(line_length - door_size - before_door - before_door * 2.5)
     maze_maker.right(90)
     line_length += 20
     numlines += 1
-
-
-
-
-
 
 
 wn.listen()
